8c261f168cb5ea2bcf4b92915f8ba5900f939e71
Brishna Bakshev <b.bakshev@gmail.com>
2023-02-10 11:52:04 -0800

    added the business logic to calculate the current ampount into the exchanged amount

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 import './css/styles.css';
 import ExchangeRate from './exchange-rate.js';
 
+// Business Logic
 function getExchangeRate(currentAmount, exchangedAmount) {
   ExchangeRate.getExchangeRate()
     .then(function(response) {
       if (response.result === "success") {
-        printExchange(currentAmount, exchangedAmount);
+        const exchangeRate = response.conversion_rates[exchangedAmount];
+        const convertedAmount = currentAmount * exchangeRate;
+        printExchange(currentAmount, convertedAmount, exchangedAmount);
       } else {
-        printError(exchangedAmount);
+        printError(response);
       }
     });
 }
 
-function printExchange(currentAmount, exchangedAmount) {
-  console.log(`${currentAmount} USD is equivalent to ${exchangedAmount} in another currency.`);
+// UI Logic
+function printExchange(currentAmount, exchangedAmount, convertedAmount) {
+  document.getElementById('exchanged-rate').innerHTML = `${currentAmount} USD is equivalent to ${convertedAmount} ${exchangedAmount}.`;
 }
 
-function printError() {
-  console.error("An error occurred while trying to retrieve the exchange rate.");
+function printError(error) {
+  document.getElementById('exchanged-rate').innerHTML = error;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const currentAmount = document.getElementById('amount').value;
+  const exchangedAmount = document.getElementById('currency-to').value.toUpperCase();
+  getExchangeRate(currentAmount, exchangedAmount);
+
 }
 
 window.addEventListener("load", function(){
-  getExchangeRate();
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
 });
